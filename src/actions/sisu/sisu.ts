fd42a432d3364d3d12461c1233d89798808f9c16
Daniel Botero <daniel@sisudata.com>
2022-04-19 08:45:58 -0700

    add create metric

--- src/actions/sisu/sisu.ts
+++ src/actions/sisu/sisu.ts
@@ -1,11 +1,6 @@
 import * as Hub from "../../hub"
 import axios from "axios"
 
-interface AxiosConfig {
-  headers: {
-    Authorization: string;
-  };
-}
 export class SisuAction extends Hub.Action {
 
   name = "sisu"
@@ -25,23 +20,12 @@ export class SisuAction extends Hub.Action {
   ]
 
   async execute(request: Hub.ActionRequest) {
-    // const stringifyRequest = JSON.stringify(request)
-    const connectionId: string = request.formParams.connection || ''
-    const requestSQL: string = request.attachment?.dataJSON.sql
-    const runAt: string = request.attachment?.dataJSON.runat
-    const tableName = request.scheduledPlan?.query?.view || request.scheduledPlan?.query?.model || ''
-    const queryName = `${request.scheduledPlan?.title} - ${runAt}` || `New Metrics - ${runAt}`
-    const axiosConfig = this.getAxiosConfig(request)
-
     try {
-      const tables = await axios.get(`https://dev.sisu.ai/rest/connections/${connectionId}/tables`, axiosConfig)
-      const tableDB = this.findTableDB(tables.data.tables, tableName)
-      if (!tableDB) {
-        throw "Wasn't able to find a table in Sisu."
-      }
-      const newSQL = requestSQL.slice(0, requestSQL.indexOf('FROM') + 'FROM'.length) + ` "${tableDB}".` + requestSQL.slice(requestSQL.indexOf('FROM') + ('FROM'.length + 1))
-      const baseQuery = await this.createQuery(newSQL, queryName, connectionId, axiosConfig)
-      console.log('--- baseQuery:', baseQuery)
+      const tableDB = await this.getTableDB(request)
+      const sisuBaseQuery = this.buildSisuBaseQuery(request, tableDB)
+      const baseQuery= await this.createQuery(request, sisuBaseQuery)
+      const metric = await this.createMetric(request, baseQuery.base_query_id)
+      console.log('--- metric:', metric)
 
       return new Hub.ActionResponse({ success: true })
     } catch (error) {
@@ -71,14 +55,88 @@ export class SisuAction extends Hub.Action {
     return form
   }
 
-  private async createQuery(queryString: string, queryName: string, connectionId: string, axiosConfig: AxiosConfig) {
+  private async getTableDB(request: Hub.ActionRequest) {
+    const axiosConfig = this.getAxiosConfig(request)
+    const connectionId = request.formParams.connection
+    const tableName = request.scheduledPlan?.query?.view || request.scheduledPlan?.query?.model
+
+    // TODO - move to a validate function using enums for params
+    // and it will ahve its error messages
+    if (!connectionId) {
+      throw "User needs to select a Sisu connection"
+    }
+
+    if (!tableName) {
+      throw "There is no table name in the data"
+    }
+
     try {
-      const baseQuery = await axios.post(`https://dev.sisu.ai/rest/data_sources/${connectionId}/custom_queries`,
-        {
-          name: queryName,
-          query_string: queryString
-        }, axiosConfig)
-      return baseQuery
+      const tables = await axios.get(`https://dev.sisu.ai/rest/connections/${connectionId}/tables`, axiosConfig)
+      const tableDB = this.findTableDB(tables.data.tables, tableName)
+      if (!tableDB) {
+        throw "Wasn't able to find a table in Sisu."
+      }
+      return tableDB
+    } catch (error) {
+      throw error
+    }
+  }
+
+  private buildSisuBaseQuery(request: Hub.ActionRequest, tableDB: string) {
+    const requestSQL: string = request.attachment?.dataJSON.sql
+    if (!requestSQL) {
+      throw "There is no sql query in data"
+    }
+    return requestSQL.slice(0, requestSQL.indexOf('FROM') + 'FROM'.length) + ` "${tableDB}".` + requestSQL.slice(requestSQL.indexOf('FROM') + ('FROM'.length + 1))
+  }
+
+  private async createMetric(request: Hub.ActionRequest, baseQueryId: string) {
+    const axiosConfig = this.getAxiosConfig(request)
+    const currentTime = new Date().toISOString()
+    const metricName = `${currentTime}_${request.scheduledPlan?.title}_metric` || `${currentTime}_metric`
+    const connectionId = request.formParams.connection
+    if (!connectionId) {
+      throw "User needs to select a Sisu connection"
+    }
+    const measure = request.attachment?.dataJSON.fields.measures[0]
+    if (!measure) {
+      throw "No measures in data"
+    }
+    const metricBody = {
+      created_at: currentTime,
+      data_source_id: connectionId,
+      default_calculation: measure.type,
+      desired_direction: measure.sorted.desc ? 'decrease' : 'increase',
+      kpi_column_name: measure.sql,
+      name: metricName,
+      static_base_query_id: baseQueryId
+    }
+    
+    try {
+      const metricRequest = await axios.post('https://dev.sisu.ai/rest/metrics', metricBody, axiosConfig)
+      return metricRequest.data
+    } catch (error) {
+      throw error
+    }
+  }
+
+  private async createQuery(request: Hub.ActionRequest, queryString: string) {
+    const axiosConfig = this.getAxiosConfig(request)
+    const connectionId = request.formParams.connection
+    if (!connectionId) {
+      throw "User needs to select a Sisu connection"
+    }
+    const currentTime = new Date().toISOString()
+    const queryName = `${currentTime}_${request.scheduledPlan?.title}_query` || `${currentTime}_query`
+
+    const newBaseQuery = {
+      name: queryName,
+      query_string: queryString
+    }
+
+    try {
+      const queryRequest = await axios.post(`https://dev.sisu.ai/rest/data_sources/${connectionId}/custom_queries`, newBaseQuery, axiosConfig)
+      return queryRequest.data
     } catch (error) {
       console.error(error)
       throw "Error creating a query."
